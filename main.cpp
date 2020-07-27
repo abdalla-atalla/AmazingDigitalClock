9645be8ebd9e01602d5e87729f10fa6cac786652
abdalla-atalla <60201830+abdalla-atalla@users.noreply.github.com>
2020-07-27 12:50:55 -0700

    Update main.cpp
    Deleted comment

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 //  This program is written by Abdalla Atalla and finished on 11/8/19. This program will ask the user to input a time in the format of HH:MM then based on the time that was entered the program will display if it is a lucky or normal time based on whether or not if the sum of the hours is equal to the sum of the minutes. Then the program will tell if the time carries a special meaning such as same digits or ascending/descending order. Lastly, the program will ask if the user wishes to enter another time or exit the program.
 //  Created by Abdalla Atalla on 11/4/19.
 //  Copyright © 2019 Abdalla Atalla. All rights reserved.
-//Abdalla
 
 #include <cstdlib>
 #include <cstring>
